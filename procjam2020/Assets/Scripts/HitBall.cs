d7a0d5d353ca7f03749eaddb73528121bed857c0
TheCell <simon_hischier@hotmail.com>
2020-12-05 16:20:37 +0100

    First working ball with fixed camera

---
+++ procjam2020/Assets/Scripts/HitBall.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class HitBall : MonoBehaviour
+{
+    [SerializeField]
+    private Transform cameraTransform;
+    [SerializeField]
+    float shotPower = 10f;
+    private bool standsStill = false;
+    private Rigidbody rigidbody;
+
+    public void Start()
+    {
+        if (cameraTransform == null)
+        {
+            Debug.LogError("no camera transform set");
+        }
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    public void Update()
+    {
+        
+    }
+
+    public void FixedUpdate()
+    {
+        standsStill = false;
+        //Debug.Log(rigidbody.velocity.magnitude);
+        if (rigidbody.velocity.magnitude < 0.1f)
+        {
+            standsStill = true;
+        }
+    }
+
+    public void Fire(InputAction.CallbackContext context)
+    {
+        if (standsStill)
+        {
+            Debug.Log("Fire");
+            Vector3 shotDirection = Vector3.ProjectOnPlane(cameraTransform.forward, Vector3.up).normalized;
+            rigidbody.AddForce(shotDirection * shotPower);
+            standsStill = true;
+        }
+    }
+}
---
+++ procjam2020/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float smoothTime = 0.3f;
+    public Vector3 smoothVelocity;
+
+    [SerializeField]
+    private Transform target;
+    [SerializeField]
+    private Vector3 cameraOffset = Vector3.zero;
+
+    private void Start()
+    {
+        if (target == null)
+        {
+            Debug.LogError("missing target references");
+        }
+    }
+
+    private void Update()
+    {
+        Transform tempTransform = target;
+        tempTransform.rotation = Quaternion.identity;
+        Vector3 targetPosition = tempTransform.TransformPoint(cameraOffset);
+        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref smoothVelocity, smoothTime);
+    }
+}
